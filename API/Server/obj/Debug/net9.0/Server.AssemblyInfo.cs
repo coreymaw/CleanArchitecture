4b5ce642ca6e9d832053237ae1659a59f7d0d333
ian <ianebdao890@gmail.com>
2025-06-29 00:43:49 +0800

    Second Commit

--- API/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ API/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd7a501a4a8a75a5ff7613407a665ddbd9c55574")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50692940c3fcda468b6be32288dfd614fca2681b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
